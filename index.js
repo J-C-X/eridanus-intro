4fb17d85d0589f28af405ac0f45fd54ef3d21368
Jose Castillo <jose091202@gmail.com>
2023-03-20 19:27:25 -0700

    added skills list

--- index.js
+++ index.js
@@ -7,18 +7,25 @@ let footer = document.querySelector("footer");
 let copyright = document.createElement('p');
 copyright.innerHTML = "Jose Castillo " + thisYear;
 
+
 document.body.appendChild(copyright);
 
-let skills = ["Coding in Python", "Coding in JavaScript", "Digital Marketing", "Proficient in Excel"];
-let skillsSection = document.querySelector("#skills");
-let skillsList = skillsSection.querySelector("ul");
+//const skillsSection = document.getElementById("skills");
+//let skills = ["Coding in Python", "Coding in JavaScript", "Digital Marketing", "Proficient in Excel"];
+//let skillsList = skillsSection.querySelector("ul");
 
+const skills = document.getElementById("skills");
+const skillsList = ["Coding in Python", "Coding in JavaScript", "Digital Marketing", "Proficient in Excel"];
 //Loop for Array 
-let total = 0;
-for( i=0; i < skills.length; i++) {
-let skill = document.createElement("li");
-    total += skills[i];
-//skill.innerHTML = skills[i];
-skillsList.appendChild(skill);
-//console.log(skills[i])
+for( let skill of skillsList ) {
+    let newList = document.createElement("li");
+    newList.textContent = skill;
+//let skill = document.createElement("ul");
+//skill.textContent = skills;
+//skill.innerHTML = skills;
+
+//skills.appendChild(skill);
+    skills.appendChild(newList);
 }
+
+
