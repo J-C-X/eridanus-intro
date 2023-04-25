6778df518d82fb08138b348cf184c5ef0adf4d2e
Jose Castillo <jose091202@gmail.com>
2023-04-24 17:41:29 -0700

    4-4 Added CSS Basics

--- index.js
+++ index.js
@@ -7,18 +7,54 @@ let footer = document.querySelector("footer");
 let copyright = document.createElement('p');
 copyright.innerHTML = "Jose Castillo " + thisYear;
 
-document.body.appendChild(copyright);
+footer.appendChild(copyright);
 
-let skills = ["Coding in Python", "Coding in JavaScript", "Digital Marketing", "Proficient in Excel"];
-let skillsSection = document.querySelector("#skills");
-let skillsList = skillsSection.querySelector("ul");
+const skillsSection = document.getElementById("skills");
+const skillsList = ["Coding in Python", "Coding in JavaScript", "Digital Marketing", "Proficient in Excel"];
+let skills = skillsSection.querySelector("ul");
 
 //Loop for Array 
-let total = 0;
-for( i=0; i < skills.length; i++) {
-let skill = document.createElement("li");
-    total += skills[i];
-//skill.innerHTML = skills[i];
-skillsList.appendChild(skill);
-//console.log(skills[i])
+for( let skill of skillsList ) {
+    let newList = document.createElement("li");
+    newList.textContent = skill;
+
+    skills.appendChild(newList);
 }
+
+const messageForm = document.getElementById('leave_message');
+const messageSection = document.getElementById('messages');
+const messageList = document.getElementById('messageList');
+
+// Event Listener for form submit
+messageForm.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form from refreshing the page
+
+  // Retrieve form field values
+  const usersName = event.target.usersName.value;
+  const usersEmail = event.target.usersEmail.value;
+  const usersMessage = event.target.usersMessage.value;
+
+  // Log form field values
+  console.log('Name:', usersName);
+  console.log('Email:', usersEmail);
+  console.log('Message:', usersMessage);
+
+  // Display messages in list
+  const newMessage = document.createElement('li');
+  newMessage.innerHTML = `
+    <a href="mailto:${usersEmail}">${usersName}</a>
+    <span>${usersMessage}</span>
+  `;
+  const removeButton = document.createElement('button');
+  removeButton.innerText = 'Remove';
+  removeButton.type = 'button';
+  removeButton.addEventListener('click', function() {
+    const entry = removeButton.parentNode;
+    entry.remove();
+  });
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+
+  // Clear form fields
+  messageForm.reset();
+});
